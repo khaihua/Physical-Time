5606f4e07ef46fb2167fdf3c62994152a0b90d58
Cristian Gonzales <xcristian.gonzales@gmail.com>
2018-02-14 22:08:39 -0800

    First iteration of Cordova iOS app

--- scripts/config_changes.sh
+++ scripts/config_changes.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-
 # Author: Cristian Gonzales
 # Created for Physical Time
 
 # Quick script to remove platforms and rebuild, and running an iOS target. Change platforms and targets as needed.
+# Uncomment platforms as needed, and uncomment the "if" block to deploy on your device.
+
+# Move into root directory
+cd ..
 
 # Rebuild the platforms in the config file
 # cordova platform rm browser
@@ -14,7 +17,16 @@ cordova platform add ios
 # cordova platform add android
 
 # Build for all platforms
-cordova build
+cordova -d build
 
 # Run for this iOS target
 cordova run ios --target="iPhone-8, 11.2"
+
+# Deploying on your device (comment out the block comments and comment-out everything above this block to properly
+# execute.
+if [ 1 -eq 0 ]; then
+cordova build ios --device
+cd platforms/ios/build/device
+/usr/bin/xcrun -sdk iphoneos PackageApplication "$(pwd)/$PROJECT_NAME.app" -o "$(pwd)/$PROJECT_NAME.ipa"
+cd ../../../..
+fi
